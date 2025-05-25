7c94e9f8aa207471cb87f234c07e9b78f4485cfc
leixin <chjgreen@hotmail.com>
2025-05-25 17:04:28 +0800

    delete style.css

--- src/entrypoints/options/main.tsx
+++ src/entrypoints/options/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Options from './Options.tsx';
-import '@/assets/style.css';
 import '@/assets/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
